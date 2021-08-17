6c962a18acc8d41ed0e740a7112cdd17696493b0
dk1code <dk1code@web.de>
2021-08-17 18:40:50 +0200

    stuck on array init

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -1,9 +1,15 @@
 // export
-  const rows = num => {
-    let output = new Array(num).fill([])
-    if (num === 0) return output
+const rows = num => {
+     const output = new Array(num).fill([])
+     if (num === 0) return output
+     output[0] = [1]
+     if (num === 1) return output
+console.log(output[1]);
+  for (let i = 1; i < num; i++) {
+    output[i].push(1)
+    output[i].push(1)
+  }
+     return output
+}
 
-    return output
-};
-
-console.log(rows(5));
+console.log(rows(4))
