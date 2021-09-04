c4cca2a7b222c7c0386f654573dc5d88c7e6c241
dk1code <dk1code@web.de>
2021-09-04 21:49:13 +0200

    created an alternative algorithm using while loop

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -1,12 +1,27 @@
+// export const rows = (num = 0, row = 0, result = []) => {
+//      if (num === row) return result
+//      if (row === 0) {
+//           result.push([1])
+//      } else {
+//           result[row] = [...result[row - 1]].map((num, index, rowArray) =>
+//                num && rowArray[index - 1] ? num + rowArray[index - 1] : 1
+//           )
+//           result[row].push(1)
+//      }
+//      return rows(num, row + 1, result)
+// }
+
 export const rows = (num = 0, row = 0, result = []) => {
-     if (num === row) return result
-     if (row === 0) {
-          result.push([1])
-     } else {
-          result[row] = [...result[row - 1]].map((num, index, result) =>
-               num && result[index - 1] ? num + result[index - 1] : 1
-          )
-          result[row].push(1)
+     if (num === 0) return result
+     result.push([1])
+     row += 1
+     while (row < num) {
+          result[row] = [...result[row - 1]]
+               .map((num, index, rowArray) =>
+                    num && rowArray[index - 1] ? num + rowArray[index - 1] : 1
+               )
+               result[row].push(1)
+          row++
      }
-     return rows(num, row + 1, result)
+     return result
 }
