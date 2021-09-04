4e0ee45596c574a1a40edb4aa154862baad327d3
dk1code <dk1code@web.de>
2021-09-04 21:16:12 +0200

    renamed vars according to mentoring

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -1,12 +1,12 @@
-export const rows = (num = 0, count = 0, array = []) => {
-     if (num === count) return array
-     if (count === 0) {
-          array.push([1])
+export const rows = (num = 0, row = 0, result = []) => {
+     if (num === row) return result
+     if (row === 0) {
+          result.push([1])
      } else {
-          array[count] = [...array[count - 1]].map((num, index, array) =>
-               num && array[index - 1] ? num + array[index - 1] : 1
+          result[row] = [...result[row - 1]].map((num, index, result) =>
+               num && result[index - 1] ? num + result[index - 1] : 1
           )
-          array[count].push(1)
+          result[row].push(1)
      }
-     return rows(num, count + 1, array)
+     return rows(num, row + 1, result)
 }
