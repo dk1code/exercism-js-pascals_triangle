6be53452cf6f6f89d04fe0a0d8131272064e1da4
dk1code <dk1code@web.de>
2021-09-05 03:06:07 +0200

    third solution using Array.from() in while loop

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -3,7 +3,7 @@
 //      if (row === 0) {
 //           result.push([1])
 //      } else {
-//           result[row] = [...result[row - 1]].map((num, index, rowArray) =>
+//           result[row] = [...result[previousRow]].map((num, index, rowArray) =>
 //                num && rowArray[index - 1] ? num + rowArray[index - 1] : 1
 //           )
 //           result[row].push(1)
@@ -11,17 +11,36 @@
 //      return rows(num, row + 1, result)
 // }
 
+// export const rows = (num = 0, row = 0, result = []) => {
+//      if (num === 0) return result
+//      result.push([1])
+
+//      while (++row < num) {
+//           let previousRow = row - 1
+//           result[row] = [...result[previousRow]].map((num, index, rowArray) => {
+//                let previousNum = index - 1
+//                return num && rowArray[previousNum] ? num + rowArray[previousNum] : 1
+//           })
+//           result[row].push(1)
+//      }
+
+//      return result
+// }
+
+
+
 export const rows = (num = 0, row = 0, result = []) => {
+     const mapFunction = (num, index, previousRow = result[row - 1]) =>
+          num && previousRow[index - 1] ? num + previousRow[index - 1] : 1
+     
      if (num === 0) return result
      result.push([1])
-     row += 1
-     while (row < num) {
-          result[row] = [...result[row - 1]]
-               .map((num, index, rowArray) =>
-                    num && rowArray[index - 1] ? num + rowArray[index - 1] : 1
-               )
-               result[row].push(1)
-          row++
+
+     while (++row < num) {
+          let previousRow = result[row - 1]
+          result[row] = Array.from(previousRow, mapFunction)
+          result[row].push(1)
      }
+
      return result
 }
