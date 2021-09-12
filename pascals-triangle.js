15ef85c63eccd4f5469ea26d2ecfcd79c4159300
dk1code <dk1code@web.de>
2021-09-12 12:10:15 +0200

    forth solution in functional style

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -1,45 +1,24 @@
-// export const rows = (num = 0, row = 0, result = []) => {
-//      if (num === row) return result
-//      if (row === 0) {
-//           result.push([1])
-//      } else {
-//           result[row] = [...result[previousRow]].map((num, index, rowArray) =>
-//                num && rowArray[index - 1] ? num + rowArray[index - 1] : 1
-//           )
-//           result[row].push(1)
-//      }
-//      return rows(num, row + 1, result)
-// }
+const FIRST_ROW = [1]
 
-// export const rows = (num = 0, row = 0, result = []) => {
-//      if (num === 0) return result
-//      result.push([1])
+const nextRow = lastRow => {
+     if (!lastRow) return FIRST_ROW
+     let mapResult = lastRow.map(mapFunction)
+     return [...mapResult, 1]
+}
 
-//      while (++row < num) {
-//           let previousRow = row - 1
-//           result[row] = [...result[previousRow]].map((num, index, rowArray) => {
-//                let previousNum = index - 1
-//                return num && rowArray[previousNum] ? num + rowArray[previousNum] : 1
-//           })
-//           result[row].push(1)
-//      }
-
-//      return result
-// }
-
-
+const mapFunction = (item, index, array) => {
+     const leftValue = array[index - 1] ?? 0
+     const rightValue = item
+     return leftValue + rightValue
+}
 
 export const rows = (num = 0, row = 0, result = []) => {
-     const mapFunction = (num, index, previousRow = result[row - 1]) =>
-          num && previousRow[index - 1] ? num + previousRow[index - 1] : 1
-     
      if (num === 0) return result
-     result.push([1])
 
-     while (++row < num) {
-          let previousRow = result[row - 1]
-          result[row] = Array.from(previousRow, mapFunction)
-          result[row].push(1)
+     while (row < num) {
+          const lastRow = result[row - 1]
+          result[row] = nextRow(lastRow)
+          row++
      }
 
      return result
